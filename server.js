e12198c052fcf4c6a6a7474b9396f1edd9c59bc8
Lukmandst <lukmandwi11@gmail.com>
2022-08-07 14:20:17 +0700

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 
 const mainRouter = require("./src/routes/index");
@@ -8,7 +9,12 @@ dbConn();
 
 const app = express();
 const PORT = process.env.PORT;
-
+const corsOptions = {
+  origin: ["*", "http://localhost:3000"],
+  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mainRouter);
